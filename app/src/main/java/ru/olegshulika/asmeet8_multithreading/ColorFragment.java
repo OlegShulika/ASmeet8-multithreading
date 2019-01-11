cbfe271d6b65cfa1bbb6cfac6df3ade9054e0fd5
OlegShulika <olegshulika@gmail.com>
2019-01-11 19:47:38 +0300

    1st commit

---
+++ app/src/main/java/ru/olegshulika/asmeet8_multithreading/ColorFragment.java
@@ -0,0 +1,77 @@
+package ru.olegshulika.asmeet8_multithreading;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ColorFragment extends Fragment {
+    private static final String TAG = "ColorFragment";
+    private OnFragmentInteractionListener mListener;
+
+    public ColorFragment() {
+        // Required empty public constructor
+    }
+
+    public static ColorFragment newInstance() {
+        ColorFragment fragment = new ColorFragment();
+        return fragment;
+    }
+
+    public static ColorFragment newInstance(Bundle args) {
+        ColorFragment fragment = new ColorFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        if (getArguments() != null) {
+            // TODO
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            //throw new RuntimeException(context.toString()
+            //        + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/ru/olegshulika/asmeet8_multithreading/MainActivity.java
@@ -0,0 +1,59 @@
+package ru.olegshulika.asmeet8_multithreading;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        initViews();
+        initListeners();
+    }
+
+    private void initViews(){
+        setContentView(R.layout.activity_main);
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().add(R.id.main_activity, ColorFragment.newInstance()).commit();
+    }
+
+    private void initListeners() {
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+}
